31de6f581d9208b937f338ad56b23b6720b38ce7
vovantung <vovantungdt123@gmail.com>
2026-04-18 13:05:10 +0700

    cm60

--- main-app/src/main/java/txu/report/mainapp/api/AccountApi.java
+++ main-app/src/main/java/txu/report/mainapp/api/AccountApi.java
@@ -86,10 +86,10 @@ public class AccountApi extends AbstractApi {
         );
     }
 
-//    @PostMapping(value = "/admin/account/get-paging")
-//    public List<AccountDto> getPaging(@RequestBody AccountRequest accountRequest) {
-//        return accountService.getPaging(accountRequest.getKeyOffset(), accountRequest.getLimit(), accountRequest.getKeySearch());
-//    }
+    @PostMapping(value = "/admin/account/get-paging")
+    public List<AccountDto> getPaging(@RequestBody AccountRequest accountRequest) {
+        return accountService.getPaging(accountRequest.getKeyOffset(), accountRequest.getLimit(), accountRequest.getKeySearch());
+    }
 
     // User
     @PostMapping("/user/account/update-avatar")
